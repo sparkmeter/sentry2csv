a3406298bced60bb9f6de8c4851e4221dda71809
Aru Sahni <aru.sahni@sparkmeter.io>
2022-02-25 15:51:35 -0500

    Add Python 3.9 and 3.10 to the classifiers.

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "License :: OSI Approved :: MIT License",
